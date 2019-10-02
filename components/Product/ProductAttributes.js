ca6eafa6b25e2629e96c5e849b8595af3ac48e82
Michał Janiczko <michaljaniczko1@gmail.com>
2019-10-02 19:58:52 +0200

    Delete modal on and back - todo confim delete delete request

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,6 +1,9 @@
-import { Header, Button } from 'semantic-ui-react';
+import React, { useState } from 'react'
+import { Header, Button, Modal } from 'semantic-ui-react';
 
 function ProductAttributes({ description }) {
+  const [modal, setModal] = useState(false)
+
   return(
     <>
       <Header as='h3'>About this product</Header>
@@ -9,7 +12,26 @@ function ProductAttributes({ description }) {
         icon='trash alternate outline'
         color='red'
         content="Delete Product"
+        onClick={() => setModal(true)}
       />
+      <Modal open={modal} dimmer='blurring'>
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            content='Cancel'
+            onClick={() => setModal(false)}
+          />
+          <Button
+            negative
+            icon='trash'
+            labelPosition='right'
+            content='Delete'
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   );
 }

ddc39526012a4a35cc0dc6008aeb44e44e558c2f
Michał Janiczko <michaljaniczko1@gmail.com>
2019-10-07 21:18:01 +0200

    Base form for create new products

--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,50 @@
+import {
+  Form,
+  Input,
+  TextArea,
+  Button,
+  Image,
+  Message,
+  Header,
+  Icon
+} from 'semantic-ui-react';
+
 function CreateProduct() {
-  return <>create</>;
+  return (
+    <>
+    <Header as='h2' block>
+      <Icon name='add' color='orange' />
+      Create New Product
+    </Header>
+    <Form>
+      <Form.Group widths='equal'>
+        <Form.Field
+          control={Input}
+          name='name'
+          label='Name'
+          placeholder='Name'
+        />
+        <Form.Field
+          control={Input}
+          name='price'
+          label='Price'
+          placeholder='Price'
+          min='0.00'
+          stop='0.01'
+          type='number'
+        />
+        <Form.Field
+          control={Input}
+          name='media'
+          label='Media'
+          content='Select Image'
+          type='file'
+          accept='image/*'
+        />
+      </Form.Group>
+    </Form>
+    </>
+  )
 }
 
 export default CreateProduct;

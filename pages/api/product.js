a4c9c16e56edb06ceffac6c5bd1f0a1a9257f15a
Michał Janiczko <michaljaniczko1@gmail.com>
2019-10-09 10:19:52 +0200

    Add new product handlePostRequest

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,9 +1,15 @@
 import Product from '../../models/Product';
+import connectDb from '../../utils/connectDb';
+
+connectDb();
 
 export default async (req, res) => {
   switch (req.method) {
     case 'GET':
       await handleGetRequest(req, res);
+      break;
+    case 'POST':
+      await handlePostRequest(req, res);
       break;
     case 'DELETE':
       await handleDeleteRequest(req, res);
@@ -13,6 +19,20 @@ export default async (req, res) => {
       break;
   };
 };
+
+async function handlePostRequest(req, res) {
+  const { name, price, description, mediaUrl } = req.body
+  if(!name || !price || !description || !mediaUrl) {
+    return res.status(422).send('Product missing one or more fields');
+  };
+  const product = await new Product({
+    name,
+    price,
+    description,
+    mediaUrl
+  }).save();
+  res.status(201).json(product);
+}
 
 async function handleGetRequest(req, res) {
   const { _id } = req.query;

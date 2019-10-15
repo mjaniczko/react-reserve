05dda68ca663d9332596de8a27a8878d730ac113
Michał Janiczko <michaljaniczko1@gmail.com>
2019-10-15 12:12:39 +0200

    Components structure for signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,57 @@
+import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
+import Link from 'next/link';
+
 function Signup() {
-  return <>signup</>;
+  return <>
+    <Message
+      attached
+      icon='settings'
+      header='Get Started!'
+      content='Create a new account'
+      color='teal'
+    />
+    <Form>
+      <Segment>
+        <Form.Input
+          fluid
+          icon='user'
+          iconPosition='left'
+          label='Name'
+          placeholder='Name'
+          name='name'
+        />
+        <Form.Input
+          fluid
+          icon='envelope'
+          iconPosition='left'
+          label='Email'
+          placeholder='Email'
+          name='email'
+        />
+        <Form.Input
+          fluid
+          icon='lock'
+          iconPosition='left'
+          label='Password'
+          placeholder='Password'
+          name='password'
+        />
+        <Button
+          icon='singup'
+          type='submit'
+          color='orange'
+          content='Singup'
+        />
+      </Segment>
+    </Form>
+    <Message attached='bottom' warning>
+      <Icon name='help'/>
+      Exisiting user?{" "}
+      <Link href='/login'>
+        <a>Log in here</a>
+      </Link>{" "}instead.
+    </Message>
+  </>;
 }
 
 export default Signup;

e1e07ca9b93e6b9fea199d6ae0aefc97a3b10ce5
Michał Janiczko <michaljaniczko1@gmail.com>
2019-10-20 12:10:03 +0200

    Destroy cookie and redirect to login page if token unvalid / expired

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from "next/app";
 import Layout from "../components/_App/Layout";
-import { parseCookies } from 'nookies';
+import { parseCookies, destroyCookie } from 'nookies';
 import { redirectUser } from '../utils/auth';
 import baseUrl from '../utils/baseUrl';
 import axios from 'axios';
@@ -29,6 +29,10 @@ class MyApp extends App {
         pageProps.user = user
       } catch (error) {
         console.error('Error getting current user', error);
+        // Throw out invalid token
+        destroyCookie(ctx, 'token');
+        // Redirect to login page
+        redirectUser(ctx, '/login');
       }
     };
 

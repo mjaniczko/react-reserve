e833e2018f8f009e5981ff164b381ac91530efd3
Michał Janiczko <michaljaniczko1@gmail.com>
2019-10-20 11:05:22 +0200

    Redirect if no token else log user

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,44 @@
 import App from "next/app";
 import Layout from "../components/_App/Layout";
+import { parseCookies } from 'nookies';
+import { redirectUser } from '../utils/auth';
+import baseUrl from '../utils/baseUrl';
+import axios from 'axios';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
+    const { token } = parseCookies(ctx);
+
     let pageProps = {};
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
-    }
+    };
 
-    return { pageProps }
+    if (!token) {
+      const isProtectedRoute = ctx.pathname === '/account' || ctx.pathname === '/create'
+      if (isProtectedRoute) {
+        redirectUser(ctx, '/login');
+      };
+    } else {
+      try {
+        const payload = { headers: { Authorization: token } };
+        const url = `${baseUrl}/api/account`;
+        const response = await axios.get(url, payload);
+        const user = response.data;
+        pageProps.user = user
+      } catch (error) {
+        console.error('Error getting current user', error);
+      }
+    };
+
+    return { pageProps };
   }
 
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
+      <Layout {...pageProps} >
         <Component {...pageProps} />
       </Layout>
     );
